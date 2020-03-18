deb2ef71167e172a4e1a9fdae733483c59204083
Paul Burkart <paul_burkart_99@yahoo.com>
2020-03-18 00:01:52 -0400

    Project Reinitialization

--- 02 - Movie Seat Booking/js/script.js
+++ 02 - Movie Seat Booking/js/script.js
@@ -4,25 +4,63 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelector = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movieSelector.value;
 
+// Save Selected Movie Index
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+}
+
 // Update Totals
-function updateSelectedCount(){
+function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+    
     const selectedSeatCount = selectedSeats.length;
+
     count.innerText = selectedSeatCount;
     total.innerText = selectedSeatCount * ticketPrice;
 }
 
+// Get data from local storage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    // Update Selected seats
+    if(selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1){
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    // Update Movie Selector
+    if(selectedMovieIndex !== null) {
+        movieSelector.selectedIndex = selectedMovieIndex;
+    }
+
+}
+
 movieSelector.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount(); 
 });
 
-container.addEventListener('click', (e) =>{
+container.addEventListener('click', (e) => {
     if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
         e.target.classList.toggle('selected');
         updateSelectedCount();
     }
 });
 
+// Initial Count & Total
+updateSelectedCount();
