ed6b5756d7ded10dee4b106e62d31a638b618e76
Paul Burkart <paulburkat@protonmail.com>
2020-03-30 08:28:25 -0400

    Finishing Project 5

--- 05 - DOM Array Methods/js/script.js
+++ 05 - DOM Array Methods/js/script.js
@@ -1,9 +1,9 @@
 const main = document.getElementById('main');
-const addUserBtn = document.getElementById('add-user');
+const addUserBtn = document.getElementById('add_user');
 const doubleBtn  = document.getElementById('double');
-const showMillionairesBtn = document.getElementById('show-millionaires');
+const showMillionairesBtn = document.getElementById('show_millionaires');
 const sortBtn = document.getElementById('sort');
-const calculateWealthBtn = document.getElementById('calculate-wealth');
+const calculateWealthBtn = document.getElementById('calculate_wealth');
 
 let data = [];
 
@@ -27,7 +27,65 @@ async function getRandomUser(){
 	addData(newUser);
 }
 
+// Sort Money
+function sortMoney(){
+	data = data.sort((a,b) => b.money - a.money);
+
+	updateDOM();
+}
+
+// Filter out those with less than $1,000,000
+function showMillionaires(){
+	data = data.filter(user => user.money >= 1000000);
+	
+	updateDOM();
+}
+
+// Calculate Total Wealth
+
+function calculateWealth(){
+	const wealth = data.reduce((acc,user) => (acc += user.money), 0);
+
+	const wealthEl = document.createElement('div');
+	wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+	main.appendChild(wealthEl);
+}
+// Double Everyone's Money
+function doubleMoney(){
+	data = data.map(user => {
+		return { ...user,money: user.money * 2 };
+	});
+
+	updateDOM();
+}
+
 // Add Geneated Object to Data Array
 function addData(obj) {
 	data.push(obj);
+
+	updateDOM();
 }
+
+// Update the DOM
+function updateDOM(providedData = data){
+	main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+	providedData.forEach(item => {
+		const element = document.createElement('div');
+		element.classList.add('person');
+		element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+		main.appendChild(element);
+	});
+}
+
+// Format Number as Money
+function formatMoney(number){
+	return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g,'$&,');
+}
+
+// Event Listeners
+addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortMoney);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
